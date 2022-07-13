89ac475559f75db74bb794c998b3f8316f10ac2d
David Mehi <davidmehi@google.com>
2022-07-13 14:46:00 +0000

    Removed market pair specific files and folders

--- setup/main.tf
+++ setup/main.tf
@@ -52,7 +52,7 @@ resource "google_container_node_pool" "dyson_pool" {
   }
 
   node_config {
-    machine_type = "e2-standard-4"  
+    machine_type = "e2-standard-8"  
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
